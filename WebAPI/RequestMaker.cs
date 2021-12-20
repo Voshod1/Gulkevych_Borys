d50a9f50318cdc497fb121416141f660745f7a3d
Borys Gulkevych <Boryagul@outlook.com>
2021-12-20 22:40:16 +0200

    Add project files.

---
+++ WebAPI/RequestMaker.cs
@@ -0,0 +1,69 @@
+﻿using RestSharp;
+using System.Collections.Generic;
+
+namespace WebAPI
+{
+    class RequestMaker
+    {
+        public IRestResponse upload()
+        {
+            var client = new RestClient("https://content.dropboxapi.com/2/files/upload");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            Dictionary<string, string> headers = new Dictionary<string, string>()
+            {
+                {"Dropbox-API-Arg", "{\"path\": \"/homework/NEWfile.txt\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}" },
+                {"Content-Type", "application/octet-stream" },
+                {"Authorization", "Bearer Z4Rm6oxQw5oAAAAAAAAAAQLjWTEydjKQb3Sq95MvmvVYtps9ENu3YbSwYhVkJzLK" }
+            };
+            request.AddHeaders(headers);
+            request.AddParameter("application/octet-stream", $@"""src"" : ""IoI.txt""", ParameterType.RequestBody);
+            return client.Execute(request);
+        }
+        public IRestResponse get_file_metadata()
+        {
+
+            var client = new RestClient("https://api.dropboxapi.com/2/files/get_metadata");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            Dictionary<string, string> headers = new Dictionary<string, string>()
+            {
+                {"Content-Type", "application/json"},
+                {"Authorization", "Bearer Z4Rm6oxQw5oAAAAAAAAAAQLjWTEydjKQb3Sq95MvmvVYtps9ENu3YbSwYhVkJzLK" }
+            };
+            /*var body = @"{" + "\n" +
+                      $@"    ""path"": ""/homework/Newfile.txt""," + "\n" +
+                       @"    ""include_media_info"":  false," + "\n" +
+                       @"    ""include_deleted"": false," + "\n" +
+                       @"    ""include_has_explicit_shared_members"": false" + "\n"+
+                       @"}";*/
+            Dictionary<string, string> body = new Dictionary<string, string>()
+            {
+                {"path", "/homework/Newfile.txt"}
+            };
+            request.AddHeaders(headers);
+            request.AddJsonBody(body);
+            return client.Execute(request);
+        }
+        public IRestResponse delete()
+        {
+            var client = new RestClient("https://api.dropboxapi.com/2/files/delete_v2");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            Dictionary<string, string> headers = new Dictionary<string, string>()
+            {
+                {"Content-Type", "application/json"},
+                {"Authorization", "Bearer Z4Rm6oxQw5oAAAAAAAAAAQLjWTEydjKQb3Sq95MvmvVYtps9ENu3YbSwYhVkJzLK" }
+            };
+            string filePath = "/homework/Newfile.txt";
+            Dictionary<string, string> body = new Dictionary<string, string>()
+            {
+            {"path", filePath}
+            };
+            request.AddHeaders(headers);
+            request.AddJsonBody(body);
+            return client.Execute(request);
+
+        }
+    }
+}
---
+++ WebAPI/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace WebAPI
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var t = new RequestMaker();
+            var  upl = t.upload();
+            Assert.IsTrue(upl.IsSuccessful, upl.Content);
+        }
+        [Test]
+        public void Test2()
+        {
+            var t = new RequestMaker();
+            var upl = t.get_file_metadata();
+            Assert.IsTrue(upl.IsSuccessful, upl.Content);
+        }
+        [Test]
+        public void Test3()
+        {
+            var t = new RequestMaker();
+            var upl = t.delete();
+            Assert.IsTrue(upl.IsSuccessful, upl.Content);
+        }
+    }
+    public class IdParse
+    {
+        public string id { get; set; }
+        public string error_summary { get; set; }
+
+    }
+}
